910811489619ad970893c04ec17edb16a456ecd2
diabhalothia1 <100493743+diabhalothia1@users.noreply.github.com>
2022-11-26 00:07:38 -0800

    r

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,5 +1,13 @@
 import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.*;
+
+class Checker implements StringChecker {
+  public boolean checkString(String s) {
+      return s.contains("and");
+  }
+}
 
 public class TestListExamples {
   @Test(timeout = 100)
@@ -7,4 +15,37 @@ public class TestListExamples {
     while(true) {}
   }
   // Write your grading tests here!
+  @Test
+  public void testFilter() {
+    List<String> input1 = new ArrayList<>();
+    StringChecker sc = new Checker();
+
+    String[] newarray1 = {"command","cruel","land","treat","panda","brand"};
+    for(String t: newarray1) { input1.add(t); }
+    
+    List<String> result1 = new ArrayList<>();
+
+    String[] temp1 = {"command","land","panda","brand"};
+    for(String t: temp1) { result1.add(t); }
+
+    assertEquals(result1, ListExamples.filter(input1, sc));
+}
+
+@Test
+public void testMerge() {
+
+    List<String> array1 = new ArrayList<>();
+    String[] tempArray1 = {"apple","cat","elephant","goat","zebra"};
+    for(String t: tempArray1) { array1.add(t); }
+    
+    List<String> array2 = new ArrayList<>();
+    String[] tempArray2 = {"bat","donkey","fox","hippo","kangroo","parrot","racoon"};
+    for(String t: tempArray2) { array2.add(t); }
+
+    List<String> result1 = new ArrayList<>();
+    String[] Answer1 = {"apple","bat","cat","donkey","elephant","fox","goat","hippo","kangroo","parrot","racoon","zebra"};
+    for(String t: Answer1) { result1.add(t); }
+
+    assertEquals(result1, ListExamples.merge(array1, array2));
+  }
 }
--- grade.sh
+++ grade.sh
@@ -2,7 +2,44 @@
 
 rm -rf student-submission
 git clone $1 student-submission
-echo 'Finished cloning'
+GRADE="0"
+
+# check if ListExamples.java exists
+cd student-submission
+if [ -f ListExamples.java ]
+then
+    echo "ListExamples.java file found"
+else
+    echo "ListExamples.java file not found"
+    echo "Grade is: "$GRADE
+    exit 1
+fi
+
+cd ..
+cp -r lib student-submission/
+cp TestListExamples.java student-submission/
+cd student-submission
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+
+# check that the compile went fine
+if [ $? -eq 00 ]
+then
+    echo "Compilation succeeded"
+else
+    echo "Compilation failed"
+    echo "Grade is: "$GRADE
+    exit 1
+fi
+
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
+
+if [ $? -eq 00 ]
+then
+    GRADE=2
+    echo "Grade is: "$GRADE
+else
+    echo "Grade is: "$GRADE
+fi
 
 
 
